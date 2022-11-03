f59c3d1d40c2c60acec0da810c89cc8d3b784552
Nina Jhunjhnuwala <ninajhunjhnuwala@Ninas-MacBook-Pro.local>
2022-11-03 12:11:08 +0100

    refactor to use axios, async fetching

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const { writeFile } = require("fs");
 const { join } = require("path");
 
-//all 3 packages have vulerbilties
 // let request = require("request"); //deprecated package use axios instead
 const axios = require("axios");
 const mergeImg = require("merge-img");
@@ -17,11 +16,9 @@ let {
   size = 100,
 } = argv;
 
-//to-do: refactor - dont need obj, redundant
-
 let firstReq = {
   // https://cataas.com/cat/says/Hi%20There?width=500&amp;height=800&amp;c=Cyan&amp;s=150
-  //Use template literal instead -> modern ES6
+  //Use template literals instead -> modern ES6
   url: `https://cataas.com/cat/says/${greeting}?width=${width}&height=${height}&color=${color}&s={size}`,
   encoding: "binary",
 };
@@ -31,31 +28,29 @@ let secondReq = {
   encoding: "binary",
 };
 
+//use async to fetch both photos without blocking
 const fetchPhotos = async (photo1, photo2) => {
   try {
-    return await Promise.all([axios.get(photo1.url), axios.get(photo2.url)]);
+    return await Promise.all([
+      axios.get(photo1.url, { responseType: "arraybuffer" }),
+      axios.get(photo2.url, { responseType: "arraybuffer" }),
+    ]).then((values) => {
+      values.map((value) =>
+        console.log(`Received response with status: ${value.status}`)
+      );
+      return values;
+    });
   } catch {
     throw Error("Promise failed");
   }
 };
 
-// console.log(fetchPhotos(firstReq, secondReq));
 fetchPhotos(firstReq, secondReq).then((data) => {
-  // console.log(data);
   const [firstBody, secondBody] = data;
 
-  console.log("typeof firstBody.data: ", typeof firstBody.data);
-  // const test = Buffer.from(firstBody.data);
-  // console.log(12, typeof test, test);
-  // console.log(1, firstBody);
-
-  //________
-
-  // { src: new Buffer(firstBody, "binary"), x: 0, y: 0 },
-  // { src: new Buffer(secondBody, "binary"), x: width, y: 0 },
   mergeImg([
-    { src: Buffer.from(firstBody.data), x: 0, y: 0 }, //offsetY
-    { src: Buffer.from(secondBody.data), x: width, y: 0 },
+    { src: firstBody.data, x: 0, y: 0 },
+    { src: secondBody.data, x: width, y: 0 },
   ]).then((img) => {
     img.getBuffer("image/jpeg", (err, buffer) => {
       if (err) {
