142bd533edefb17900ebec18f2e088929e9cb384
Nina Jhunjhnuwala <ninajhunjhnuwala@Ninas-MacBook-Pro.local>
2022-11-03 13:45:42 +0100

    refactor to add createPhotoRequest, savePhotos, mergePhotos, refactor fetchPhotos

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const { writeFile } = require("fs");
 const { join } = require("path");
 
-// let request = require("request"); //deprecated package use axios instead
+// let request = require("request"); //deprecated package use axios as alternative
 const axios = require("axios");
 const mergeImg = require("merge-img");
 const argv = require("minimist")(process.argv.slice(2));
@@ -16,54 +16,61 @@ let {
   size = 100,
 } = argv;
 
-let firstReq = {
+// create function to reduce redundant code
+const createPhotoRequest = (text) => {
   // https://cataas.com/cat/says/Hi%20There?width=500&amp;height=800&amp;c=Cyan&amp;s=150
-  //Use template literals instead -> modern ES6
-  url: `https://cataas.com/cat/says/${greeting}?width=${width}&height=${height}&color=${color}&s={size}`,
-  encoding: "binary",
-};
-
-let secondReq = {
-  url: `https://cataas.com/cat/says/${who}?width=${width}&height=${height}&color=${color}&s={size}`,
-  encoding: "binary",
+  return axios.get(
+    `https://cataas.com/cat/says/${text}?width=${width}&height=${height}&color=${color}&s={size}`,
+    { responseType: "arraybuffer" }
+  );
 };
 
 //use async to fetch both photos without blocking
-const fetchPhotos = async (photo1, photo2) => {
+const fetchPhotos = async () => {
   try {
     return await Promise.all([
-      axios.get(photo1.url, { responseType: "arraybuffer" }),
-      axios.get(photo2.url, { responseType: "arraybuffer" }),
-    ]).then((values) => {
-      values.map((value) =>
-        console.log(`Received response with status: ${value.status}`)
-      );
-      return values;
-    });
+      createPhotoRequest(greeting),
+      createPhotoRequest(who),
+    ]);
   } catch {
     throw Error("Promise failed");
   }
 };
 
-fetchPhotos(firstReq, secondReq).then((data) => {
-  const [firstBody, secondBody] = data;
-
-  mergeImg([
-    { src: firstBody.data, x: 0, y: 0 },
-    { src: secondBody.data, x: width, y: 0 },
-  ]).then((img) => {
-    img.getBuffer("image/jpeg", (err, buffer) => {
+const savePhoto = async (img) => {
+  img.getBuffer("image/jpeg", (err, buffer) => {
+    if (err) {
+      console.log(err);
+    }
+    const fileOut = join(process.cwd(), `/cat-card.jpg`);
+    writeFile(fileOut, buffer, "binary", (err) => {
       if (err) {
         console.log(err);
+        return;
       }
-      const fileOut = join(process.cwd(), `/cat-card.jpg`);
-      writeFile(fileOut, buffer, "binary", (err) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        console.log("The file was saved!");
-      });
+      console.log("The file was saved!");
     });
   });
-});
+
+  // @@@@
+  // @@ Note: I would refactor to Async/Await but getBufferAsync doesn't work in the version
+  // @@ of Jimp used by mergeImg, due to the scope of the exercise, I left as promise chaining.
+  // @@@@@
+  // const imgBuffer = await img.getBufferAsync("image/jpeg");
+  // const fileOut = join(process.cwd(), `/cat-card.jpg`);
+  // writeFile(fileOut, imgBuffer, "binary");
+  // savePhoto(img);
+};
+
+const mergePhotos = async () => {
+  const [firstBody, secondBody] = await fetchPhotos();
+
+  const img = await mergeImg([
+    { src: firstBody.data, x: 0, y: 0 },
+    { src: secondBody.data, x: width, y: 0 },
+  ]);
+
+  savePhoto(img);
+};
+
+mergePhotos();
